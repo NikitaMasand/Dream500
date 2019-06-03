18b3a90932113dccddace43bb1c07894e616e664
Nikita <43334750+NikitaMasand@users.noreply.github.com>
2019-06-03 10:26:41 +0530

    Update Kadane's.java

--- Arrays/Order-Statistics/Kadane's.java
+++ Arrays/Order-Statistics/Kadane's.java
@@ -1,15 +1,31 @@
+//printing max subarray sum and their indices
+//kadane's algorithm
+
+
 public class kadanes {
     public static void main(String[] args) {
         int arr[] = new int[]{1,-3,2,-5,7,6,-1,-4,11,-23};
         System.out.println(maxSumSubarray(arr));
     }
     public static int maxSumSubarray(int arr[]){
+        int start = 0;
+        int end = 0;
         int curr = arr[0];
-        int global = arr[0];
+        int global = Integer.MIN_VALUE;
         for(int i=1;i<arr.length;i++){
-            curr = Math.max(arr[i],curr+arr[i]);
-            global = Math.max(global,curr);
+            if(arr[i]>curr+arr[i]){
+                curr = arr[i];
+                start = i;
+            }
+            else{
+                curr = curr+arr[i];
+            }
+            if(global<curr){
+                global = curr;
+                end = i;
+            }
         }
+        System.out.println("Starting from: "+ start+" Ending at: "+end);
         return global;
     }
 }
