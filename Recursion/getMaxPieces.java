9a08da77a1ae237df27c5b49db54f60641288980
Nikita <43334750+NikitaMasand@users.noreply.github.com>
2019-06-10 11:04:38 +0530

    Create getMaxPieces.java

---
+++ Recursion/getMaxPieces.java
@@ -0,0 +1,25 @@
+/*
+given a rope of length n and three values a,b,c. We need to make max no. of pieces such that
+every piece had length in set a,b,c
+ */
+
+public class getMaxPieces {
+    public static void main(String[] args) {
+        System.out.println(getMax(5,1,2,3));
+        System.out.println(getMax(23,9,11,12));
+        System.out.println(getMax(5,2,4,6));
+    }
+    public static int getMax(int n, int a, int b, int c){
+        if(n<0)
+            return -1;
+        if(n==0)
+            return 0;
+        int CA = getMax(n-a,a,b,c);
+        int CB = getMax(n-b,a,b,c);
+        int CC = getMax(n-c,a,b,c);
+        int res = Math.max(Math.max(CA,CB),CC);
+        if(res==-1)
+            return -1;
+        return res+1;
+    }
+}
