0eeee35bc56e6c90d7bc390043136a5fb8a238fd
Nikita <43334750+NikitaMasand@users.noreply.github.com>
2019-06-10 11:08:28 +0530

    Update FactorialTailRecursion.java

--- Recursion/FactorialTailRecursion.java
+++ Recursion/FactorialTailRecursion.java
@@ -1,31 +1,24 @@
-/*
-given a rope of length n and three values a,b,c. We need to make max no. of pieces such that
-every piece had length in set a,b,c
- */
-
-public class getMaxPieces {
+public class FactorialTailRecursion {
     public static void main(String[] args) {
-        System.out.println(getMax(5,1,2,3));
-        System.out.println(getMax(23,9,11,12));
-        System.out.println(getMax(5,2,4,6));
+        int k = 20;
+        System.out.println(fact(k));
+        System.out.println(factTailRecursion(k,1));
     }
-    public static int getMax(int n, int a, int b, int c){
-        if(n<0)
-            return -1;
-        if(n==0)
-            return 0;
-        int CA = getMax(n-a,a,b,c);
-        int CB = getMax(n-b,a,b,c);
-        int CC = getMax(n-c,a,b,c);
-        int res = Math.max(Math.max(CA,CB),CC);
-        if(res==-1)
-            return -1;
-        return res+1;
+    public static long fact(int n){
+        if(n==1 || n==0)
+            return 1;
+        return n*fact(n-1);
+    }
+
+    //tail recursion
+    //as the function call is the last statement
+    //explained it in google doc.
+    public static long factTailRecursion(int n, long result){
+        if(n==1 || n==0){
+            return result;
+        }
+        return factTailRecursion(n-1,n*result);
     }
 }
-/*
-Time complexity
-T(n) = T(n-a) + T(n-b) + T(n-c) + c1
-O(3^n)
 
- */
+//here both work the same in results, however tail rec. one uses constant space
