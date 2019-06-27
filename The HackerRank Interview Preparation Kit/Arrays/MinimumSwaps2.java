c5fcc847a3f6ef11472e939e74883be07fc6b4cb
Nikita <43334750+NikitaMasand@users.noreply.github.com>
2019-06-27 17:11:03 +0530

    Create MinimumSwaps2.java

---
+++ The HackerRank Interview Preparation Kit/Arrays/MinimumSwaps2.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    // Complete the minimumSwaps function below.
+    static int minimumSwaps(int[] arr) {
+        int n = arr.length;
+        int i,j,cycle_length,swaps=0;
+        boolean visited[] = new boolean[n];
+        for(i=0;i<n;i++){
+                j=i;
+                cycle_length = 0;
+                while(!visited[j]==true){
+                    visited[j]=true;
+                    j=arr[j]-1;
+                    cycle_length++;
+                }
+                if(cycle_length!=0){
+                    swaps+=cycle_length-1;
+                }
+        }
+        return swaps;
+
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] arr = new int[n];
+
+        String[] arrItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arrItem = Integer.parseInt(arrItems[i]);
+            arr[i] = arrItem;
+        }
+
+        int res = minimumSwaps(arr);
+
+        bufferedWriter.write(String.valueOf(res));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
