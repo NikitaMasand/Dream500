4ac91ff9e78ddaa353471432daffa7b23c83111f
Nikita <43334750+NikitaMasand@users.noreply.github.com>
2019-08-01 15:37:12 +0530

    Update BFSTraversal.java

--- Graphs/BFSTraversal.java
+++ Graphs/BFSTraversal.java
@@ -5,6 +5,22 @@
 */
 class Traversal
 {
+  
+  //for disconnected components in the graph, we make the boolean visited arr global and check if that vertex's boolean is false even 
+  //after one complete bfs from source, that means that vertex is disconnected and there might be others as well. to handle this
+  
+  /*
+  boolean visited[] = new visited[v];
+    public static void mainBFS(){
+      
+      for(int i=0;i<v;i++){
+        if(!visited[i]){
+            BFS(i,list,visited);
+        }
+      }
+    }
+  
+  */
     static void bfs(int s, ArrayList<ArrayList<Integer>> list, boolean visited[])
     {
        Queue<Integer> q = new LinkedList<>();
